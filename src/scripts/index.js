0de20a91a0b36377f531a0c5566a1cd7e40496bc
MutiaDianSafitri <132378919+MutiaDianSafitri@users.noreply.github.com>
2023-10-25 22:51:27 +0700

    Add files via upload

---
+++ src/scripts/index.js
@@ -0,0 +1,20 @@
+import '../stylesmain.css';
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.oclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if (window.scroll > 60) {
+        document.querySelector('#scroll-top').classList.add('active');
+    } else {
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+}
